13060ef858bd203bd0511a088d76f07781ee4761
Сокол Егор Сергеевич <sokoles@rncb.ru>
2022-12-09 14:14:03 +0300

    сваггер.

--- UserStorrage6/Program.cs
+++ UserStorrage6/Program.cs
@@ -46,12 +46,10 @@ builder.Services
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseRouting();
 
 app.UseEndpoints(endpoints =>
